f6fb54efda20e9bc47b044e5ff5ae1fbeee25ce5
MayceAlazzawi <maynab4@gmail.com>
2023-03-14 16:33:11 +0300

    adding the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("female");
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?gender=${gender}`)
+      .then((data) => data.json())
+      .then((data) => setName(data.results[0].name.first));
+  }, [gender]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello {name}</p>
+      <input
+        type="checkbox"
+        onChange={(e) => setGender(e.target.checked ? "male" : "female")}
+      />
+      Male
     </div>
   );
 }
 
 export default App;
+
+// useEffect(() => {
+//   fetch(`https://randomuser.me/api/?gender=${gender}`)
+//     .then((data) => data.json())
+//     .then((data) => setName(data.results[0].name.first));
+// }, []);
+
+{
+  /* <input
+        type="checkbox"
+        onChange={(e) => setGender(e.target.checked ? "male" : "female")}
+      />
+      Male */
+}
